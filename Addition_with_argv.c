ca091d3fe60a4233cc6191cab61fc47b3cc4d4c4
Sayantan <sayantandatta97@gmail.com>
2018-04-27 21:56:21 +0530

    Update Addition_with_argv.c

--- Addition_with_argv.c
+++ Addition_with_argv.c
@@ -4,8 +4,9 @@
 int main(int argc, char* argv[])
 {
   int i,sum=0;       //initializing values
-  printf("ARGC = %d\n",argc ); // argc is 
-  for (i=1;i<argc;i++){
+  printf("ARGC = %d\n",argc );   // argc is counting how many arguments are given in cli
+  for (i=1 ; i<argc ; i++)          // a simple loop for counting & adding the numbers one by one from cli arg
+  {
     printf("ARGV[%d] = %s\n",i,argv[i] );
     sum += atoi(argv[i]);
   }
