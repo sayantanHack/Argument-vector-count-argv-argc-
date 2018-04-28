bceb3acc3ddfe26816a6c04dc9bac15d119d2e7a
Sayantan <sayantandatta97@gmail.com>
2018-04-28 11:48:32 +0530

    Update show_args.c

--- show_args.c
+++ show_args.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>         
 
 int main(int argc, char* argv[])
 {
   int i;
   if(argc==1){
-    printf("Only one arg is - %s\n",argv[0] );
+    printf("Only one arg is - %s\n",argv[0] );     // if there only one args means only printing of the program.
   }
   else{
-    for(i=1;i<argc;i++){
-      printf("The other args are - %c\n",argv[i] );
+    for(i=1;i<argc;i++)         
+    {   
+      printf("The other args are - %c\n",argv[i] );       //printing other arguments if passed
     }
   }
 }
